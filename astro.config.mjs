9686dbb0e8e4b96a11954594bbbf8a2315e95f92
Irving Dinh <irving.dinh@gmail.com>
2025-09-16 15:21:23 +0700

    Initial commit (cont.)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
 import preact from "@astrojs/preact";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [preact()],
   vite: {
     plugins: [tailwindcss()],
