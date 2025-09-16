e974c7f4cd772591f1d42855861a45e247ed9e44
Irving Dinh <irving.dinh@gmail.com>
2025-09-16 15:01:45 +0700

    Initial commit (cont.)

---
+++ src/components/Widget.tsx
@@ -0,0 +1,11 @@
+export const Widget = () => {
+  return (
+    <div class="bg-white rounded-xl p-4 aspect-square dark:bg-black">
+      <div>
+        <p class="font-bold">AI Rewrite</p>
+      </div>
+
+      <p>Hi!</p>
+    </div>
+  );
+};
